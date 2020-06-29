081d2920cf941ea8a58272405003aa200b9f3b2b
nicolaslazzos <nicolaslazzos09@gmail.com>
2020-06-28 23:16:26 -0300

    connecting to mongo and creating api router

---
+++ routes/api/profile.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+
+// @route   GET api/users/
+// @desc    test route
+// @access  public
+router.get('/', (req, res) => res.send('profile route'));
+
+module.exports = router;
